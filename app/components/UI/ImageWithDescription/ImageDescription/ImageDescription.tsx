e16c4628669c76fb775ea1b6adf24b9725189525
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-13 03:11:46 -0400

    Update UI

--- app/components/UI/ImageWithDescription/ImageDescription/ImageDescription.tsx
+++ app/components/UI/ImageWithDescription/ImageDescription/ImageDescription.tsx
@@ -7,7 +7,9 @@ const ImageDescription = ({ image }: { image: FeatureImage }) => {
       id={image.id}
       className="flex gap-2 p-5  flex-col justify-center items-center 2xl:items-start 2xl:flex-row 2xl:odd:flex-row-reverse "
     >
-      <MultiTabsImageCarousel images={image.multiScreenImages!} />
+      {image.multiScreenImages!.length > 0 && (
+        <MultiTabsImageCarousel images={image.multiScreenImages!} />
+      )}
 
       <div className="lg:p-4 flex-1 gap-3 flex flex-col items-center text-justify lg:items-start rounded text-sm  lg:text-base   !leading-[40px]">
         <h3>{image.label}</h3>
