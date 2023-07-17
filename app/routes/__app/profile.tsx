da32a9b530fbe0869bfb0f71eeb8d6209ee22234
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-07-17 02:57:09 -0400

    update website layout

--- app/routes/__app/profile.tsx
+++ app/routes/__app/profile.tsx
@@ -1,4 +1,4 @@
-import { Link, useOutlet } from "@remix-run/react";
+import { Link, useMatches, useOutlet } from "@remix-run/react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useLocation } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
@@ -6,6 +6,8 @@ import { getUserFromSession } from "~/utils/database/auth.server";
 import { redirect } from "@remix-run/node";
 const Profile = () => {
   const location = useLocation();
+  const matches = useMatches();
+  const rootData = matches[0].data;
   const container1 = {
     hidden: { opacity: 0, x: -50 },
     show: {
@@ -28,9 +30,30 @@ const Profile = () => {
   };
   const outlet = useOutlet();
   return (
-    <div className=" flex flex-col self-center gap-10 text-slate-600 w-full h-full">
-      <div>
-        <h2 className="text-center">My Profile</h2>
+    <div className="flex self-center gap-10  w-full h-full">
+      <div className="w-[400px] p-4 rounded-lg text-indigo-800 bg-white">
+        {rootData.userData && (
+          <div className="flex flex-col gap-3">
+            <Link
+              className="  flex justify-center  flex-1 gap-2 items-center border border-indigo-400 p-2 rounded-md hover:bg-indigo-500 hover:text-white"
+              to="/profile"
+            >
+              Edit Profile
+            </Link>
+            <Link
+              className="  flex justify-center  flex-1 gap-2 items-center border border-indigo-400 p-2 rounded-md hover:bg-indigo-500 hover:text-white"
+              to="new-project"
+            >
+              New Project
+            </Link>
+            <Link
+              className=" flex-1 justify-center  flex gap-2 items-center border border-indigo-400 p-2 rounded-md  hover:bg-indigo-500 hover:text-white"
+              to="technology"
+            >
+              Technology
+            </Link>
+          </div>
+        )}
       </div>
 
       <AnimatePresence mode="wait" initial={false}>
