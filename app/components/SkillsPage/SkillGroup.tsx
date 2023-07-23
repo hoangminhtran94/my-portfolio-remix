7404ced1c1eb0d4be5f5c9f43aebeddf306b37c1
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-07-23 01:45:26 -0400

    update hover card

--- app/components/SkillsPage/SkillGroup.tsx
+++ app/components/SkillsPage/SkillGroup.tsx
@@ -1,8 +1,7 @@
 import ProjectIcon from "../UI/ProjectIcon/ProjectIcon";
-import type { FC } from "react";
-import { useRef, useEffect, useState } from "react";
+import { useRef, type FC } from "react";
 import type { Technology, TechnologyGroup } from "~/utils/models/models";
-import { useInView } from "framer-motion";
+import { useSpring, animated, config } from "react-spring";
 interface SkillGroupProps {
   skillGroup: TechnologyGroup;
   once?: boolean;
@@ -14,292 +13,39 @@ const SkillGroup: FC<SkillGroupProps> = ({
   once = false,
 }) => {
   const ref = useRef<HTMLDivElement>(null);
-  const isInView = useInView(ref, { once });
-  const [tr1Hover, setTr1Hover] = useState(false);
-  const [tr2Hover, setTr2Hover] = useState(false);
-  const [tr3Hover, setTr3Hover] = useState(false);
-  const [tr4Hover, setTr4Hover] = useState(false);
-  const [tr5Hover, setTr5Hover] = useState(false);
-  const [tr6Hover, setTr6Hover] = useState(false);
-  const [tr7Hover, setTr7Hover] = useState(false);
-  const [tr8Hover, setTr8Hover] = useState(false);
-  const [tr9Hover, setTr9Hover] = useState(false);
-  const [tr10Hover, setTr10Hover] = useState(false);
-  const [tr11Hover, setTr11Hover] = useState(false);
-  const [tr12Hover, setTr12Hover] = useState(false);
-  const [tr13Hover, setTr13Hover] = useState(false);
-  const [tr14Hover, setTr14Hover] = useState(false);
-  const [tr15Hover, setTr15Hover] = useState(false);
-  const [tr16Hover, setTr16Hover] = useState(false);
-  const [tr17Hover, setTr17Hover] = useState(false);
-  const [tr18Hover, setTr18Hover] = useState(false);
-  const [tr19Hover, setTr19Hover] = useState(false);
-  const [tr20Hover, setTr20Hover] = useState(false);
-  const [tr21Hover, setTr21Hover] = useState(false);
-  const [tr22Hover, setTr22Hover] = useState(false);
-  const [tr23Hover, setTr23Hover] = useState(false);
-  const [tr24Hover, setTr24Hover] = useState(false);
-  const [tr25Hover, setTr25Hover] = useState(false);
-  const hoverClassName = [
-    { condition: tr1Hover, className: "box-hover-card-tr-1" },
-    { condition: tr2Hover, className: "box-hover-card-tr-2" },
-    { condition: tr3Hover, className: "box-hover-card-tr-3" },
-    { condition: tr4Hover, className: "box-hover-card-tr-4" },
-    { condition: tr5Hover, className: "box-hover-card-tr-5" },
-    { condition: tr6Hover, className: "box-hover-card-tr-6" },
-    { condition: tr7Hover, className: "box-hover-card-tr-7" },
-    { condition: tr8Hover, className: "box-hover-card-tr-8" },
-    { condition: tr9Hover, className: "box-hover-card-tr-9" },
-    { condition: tr10Hover, className: "box-hover-card-tr-10" },
-    { condition: tr11Hover, className: "box-hover-card-tr-11" },
-    { condition: tr12Hover, className: "box-hover-card-tr-12" },
-    { condition: tr13Hover, className: "box-hover-card-tr-13" },
-    { condition: tr14Hover, className: "box-hover-card-tr-14" },
-    { condition: tr15Hover, className: "box-hover-card-tr-15" },
-    { condition: tr16Hover, className: "box-hover-card-tr-16" },
-    { condition: tr17Hover, className: "box-hover-card-tr-17" },
-    { condition: tr18Hover, className: "box-hover-card-tr-18" },
-    { condition: tr19Hover, className: "box-hover-card-tr-19" },
-    { condition: tr20Hover, className: "box-hover-card-tr-20" },
-    { condition: tr21Hover, className: "box-hover-card-tr-21" },
-    { condition: tr22Hover, className: "box-hover-card-tr-22" },
-    { condition: tr23Hover, className: "box-hover-card-tr-23" },
-    { condition: tr24Hover, className: "box-hover-card-tr-24" },
-    { condition: tr25Hover, className: "box-hover-card-tr-25" },
-  ].find((item) => item.condition)?.className;
+  const calc = (x: number, y: number) => [
+    -(
+      250 - Math.abs(y - (ref.current?.getBoundingClientRect().top as number))
+    ) / 500,
+    (250 -
+      Math.abs(x - (ref.current?.getBoundingClientRect().left as number))) /
+      500,
+    1,
+  ];
+  const trans = (x: number, y: number, s: number) =>
+    `perspective(1000px) rotateX(${x * 30}deg) rotateY(${
+      y * 30
+    }deg) scale(${s})`;
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { tension: 300, friction: 10 },
+  }));
 
   return (
-    <div className="box-hover noselect w-screen mx-auto">
-      <div className="box-hover-canvas">
+    <animated.div
+      ref={ref}
+      className="box-hover"
+      onMouseMove={({ clientX: x, clientY: y }) => {
+        console.log(x);
+        set({ xys: calc(x, y) });
+      }}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{
+        transform: props.xys.to(trans),
+      }}
+    >
+      <div id="box-hover-card" className={`!h-full`}>
         <div
-          onMouseEnter={() => {
-            setTr1Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr1Hover(false);
-          }}
-          className="tracker tr-1"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr2Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr2Hover(false);
-          }}
-          className="tracker tr-2"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr3Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr3Hover(false);
-          }}
-          className="tracker tr-3"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr4Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr4Hover(false);
-          }}
-          className="tracker tr-4"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr5Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr5Hover(false);
-          }}
-          className="tracker tr-5"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr6Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr6Hover(false);
-          }}
-          className="tracker tr-6"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr7Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr7Hover(false);
-          }}
-          className="tracker tr-7"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr8Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr8Hover(false);
-          }}
-          className="tracker tr-8"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr9Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr9Hover(false);
-          }}
-          className="tracker tr-9"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr10Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr10Hover(false);
-          }}
-          className="tracker tr-10"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr11Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr11Hover(false);
-          }}
-          className="tracker tr-11"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr12Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr12Hover(false);
-          }}
-          className="tracker tr-12"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr13Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr13Hover(false);
-          }}
-          className="tracker tr-13"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr14Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr14Hover(false);
-          }}
-          className="tracker tr-14"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr15Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr15Hover(false);
-          }}
-          className="tracker tr-15"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr16Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr16Hover(false);
-          }}
-          className="tracker tr-16"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr17Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr17Hover(false);
-          }}
-          className="tracker tr-17"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr18Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr18Hover(false);
-          }}
-          className="tracker tr-18"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr19Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr19Hover(false);
-          }}
-          className="tracker tr-19"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr20Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr20Hover(false);
-          }}
-          className="tracker tr-20"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr21Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr21Hover(false);
-          }}
-          className="tracker tr-21"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr22Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr22Hover(false);
-          }}
-          className="tracker tr-22"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr23Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr23Hover(false);
-          }}
-          className="tracker tr-23"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr24Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr24Hover(false);
-          }}
-          className="tracker tr-24"
-        ></div>
-        <div
-          onMouseEnter={() => {
-            setTr25Hover(true);
-          }}
-          onMouseLeave={() => {
-            setTr25Hover(false);
-          }}
-          className="tracker tr-25"
-        ></div>
-      </div>
-      <div id="box-hover-card" className={`!h-full ${hoverClassName}`}>
-        <div
-          ref={ref}
           className={`w-full  overflow-scroll h-full     rounded-xl 
                       p-[24px] lg:p-[36px]  
                       z-[888] shadow-lg flex flex-col items-center gap-[50px]`}
@@ -322,7 +68,7 @@ const SkillGroup: FC<SkillGroupProps> = ({
           </div>
         </div>
       </div>
-    </div>
+    </animated.div>
   );
 };
 
--- app/components/UI/ProjectIcon/ProjectIcon.tsx
+++ app/components/UI/ProjectIcon/ProjectIcon.tsx
@@ -10,7 +10,10 @@ const ProjectIcon: FC<ProjectIconProps> = ({ tech }) => {
       className="flex flex-col gap-3 items-center justify-center z-[201]"
       key={tech.id}
     >
-      <TechnologyIcon className="xl:w-[60px] xl:h-[60px]" icon={tech.icon} />
+      <TechnologyIcon
+        className="xl:w-[60px] xl:h-[60px] hover:animate-bouncing cursor-pointer"
+        icon={tech.icon}
+      />
       {/* <p className={`tracking-widest`}>{tech.name}</p> */}
     </li>
   );
